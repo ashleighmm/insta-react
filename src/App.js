58a2fa51da621e5986ba999afe2e81a9007d1f40
Ashleigh More-Hattia <ashleigh@hoistdigital.co.uk>
2019-05-29 16:26:10 +0100

    Created and rendered Post component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
+import Post from './components/Post'
 import Header from './components/Header';
 class App extends Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <div>
+          <Post />
+        </div>
+      </div>
     );
   }
 }
