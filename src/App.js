4635ef0717863806230cbd5c270a7c72cdb59dfe
Ashleigh More-Hattia <ashleigh@hoistdigital.co.uk>
2019-05-29 16:58:30 +0100

    Rendered Post components with mockdata

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
-import React, { Component } from 'react';
-import './App.css';
-import Post from './components/Post'
-import Header from './components/Header';
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <div>
-          <Post />
-        </div>
-      </div>
-    );
-  }
-}
-export default App;
+    // src/App.js
+
+    import React, { Component } from 'react';
+    import './App.css';
+    import Header from './components/Header';
+    import Post from './components/Post';
+
+    class App extends Component {
+      render() {
+        return <div className="App">
+            <Header />
+            <section className="App-main">
+              <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+              <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+
+              {/* more posts */}
+            </section>
+          </div>;
+      }
+    }
+
+    export default App;
